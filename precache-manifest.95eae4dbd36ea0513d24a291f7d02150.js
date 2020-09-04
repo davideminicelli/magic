bf94bf60285deaebf22d926fd714feea4367c41f
Davide Minicelli <davide.minicelli@gmail.com>
2020-09-04 12:28:20 +0100

    Updates

---
+++ precache-manifest.95eae4dbd36ea0513d24a291f7d02150.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5152b32222f76e90f086ebb5dde62e83",
+    "url": "/magic/index.html"
+  },
+  {
+    "revision": "9865074621ab3da9a1e3",
+    "url": "/magic/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "55cdb252e446e61c5797",
+    "url": "/magic/static/js/2.9444b7ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/magic/static/js/2.9444b7ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9865074621ab3da9a1e3",
+    "url": "/magic/static/js/main.bc86ab3e.chunk.js"
+  },
+  {
+    "revision": "b3f01b5ccc00494b1f58",
+    "url": "/magic/static/js/runtime-main.93fbb8f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/magic/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/magic/precache-manifest.95eae4dbd36ea0513d24a291f7d02150.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/magic/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
